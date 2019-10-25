31f3e7f82f5576a17d505d61fd28b30073c74124
Alex Rocha <rocha8524@gmail.com>
2019-10-25 18:44:48 -0400

    Fixing multiple commands

--- liri.js
+++ liri.js
@@ -3,11 +3,8 @@ require("dotenv").config();
 
 // Include the axios npm package
 var axios = require("axios");
-var bandsInTown = require("axios");
 
-// Variable for Concert-This portion of LIRI
-var concertThis = "";
-
+// Variable for keys js file
 var keys = require("./keys.js");
 
 /* Variable for Spotify portion of LIRI
@@ -15,22 +12,43 @@ var spotify = new Spotify(keys.spotify);
 */
 
 // Variable for OMBD portion of LIRI
-var movieThis = "";
+var userInput = "";
 
 // Store all arguments in an array
 var multiples = process.argv;
+var commands = process.argv[2];
 
 // Loop through all the words and do a for-loop to handle the inclusion of multiple words
 for (var i = 2; i < multiples.length; i++) {
     if (i > 2 && i < multiples.length) {
-        movieThis = movieThis + " " + multiples[i];
+        userInput = userInput + " " + multiples[i];
     } else {
-        movieThis += multiples[i];
+        userInput += multiples[i];
     }
 }
 
+/* We will then create a switch-case statement (if-else would also work).
+// The switch-case will direct which function gets run.
+switch (commands) {
+    case "movie-this":
+        userInput();
+        break;
+
+    case "concert-this":
+        concertThis();
+        break;
+
+    case "spotify-this-song":
+        spotifySong();
+        break;
+
+    case "do-what-it-says":
+        doIt();
+        break;
+*/
+
 // Run a request with axios to the OMDB API with the movie specified
-var movieUrl = "http://www.omdbapi.com/?t=" + movieThis + "&y=&plot=short&apikey=trilogy";
+var movieUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
 
 // Create a request with axios to the movieUrl
 axios.get(movieUrl).then(
@@ -42,35 +60,36 @@ axios.get(movieUrl).then(
     function (response) {
 
         // Title of the movie.
-        console.log("The title of the movie is " + movieThis);
+        console.log("The title of the movie is " + userInput);
         // Year the movie came out.
-        console.log("The movie " + movieThis + " was released in " + response.data.Year);
+        console.log("The movie " + userInput + " was released in " + response.data.Year);
         // IMDB Rating of the movie.
-        console.log("The movie " + movieThis + " IMBD rating is " + response.data.imdbRating);
+        console.log("The movie " + userInput + " IMBD rating is " + response.data.imdbRating);
         // Rotten Tomatoes Rating of the movie.
-        console.log("The movie " + movieThis + " rotten tomatoes score is " + response.data.Ratings[1].Value);
+        console.log("The movie " + userInput + " rotten tomatoes score is " + response.data.Ratings[1].Value);
         // Country where the movie was produced.
-        console.log("The movie " + movieThis + " was produced in " + response.data.Country);
+        console.log("The movie " + userInput + " was produced in " + response.data.Country);
         // Language of the movie.
-        console.log("The movie " + movieThis + " spoken language is " + response.data.Language);
+        console.log("The movie " + userInput + " spoken language is " + response.data.Language);
         // Plot of the movie.
-        console.log(movieThis + " synopsis: " + response.data.Plot);
+        console.log(userInput + " synopsis: " + response.data.Plot);
         // Actors in the movie.
-        console.log("The movie " + movieThis + " main actors are " + response.data.Actors);
+        console.log("The movie " + userInput + " main actors are " + response.data.Actors);
     });
 
-// Run a request with axios to the Bands In Town API with the artist and band specified
-var concertUrl = "https://rest.bandsintown.com/artists/" + concertThis + "/events?app_id=codingbootcamp";
+/* Run a request with axios to the Bands In Town API with the artist and band specified
+var concertUrl = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp";
 
 // Create a request with axios to the concertUrl
-bandsInTown.get(concertUrl).then (
+bandsInTown.get(concertUrl).then(
 
-        // If the request with axios is successful
-        function (music) {
+    // If the request with axios is successful
+    function (music) {
 
-            // Name of the venue
-            console.log("The " + concertThis + " next performance is at the " + music.venue.name);
+        // Name of the venue
+        console.log("The " + userInput + " next performance is at the " + music.venue.name);
 
-            // Location of the venue
+        // Location of the venue
 
-        });
+    });
+*/
